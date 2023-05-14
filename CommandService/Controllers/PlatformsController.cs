0a228de3930cc40132a4ec88ab9dbf1595317106
Vladislav Fisher <vladislav.fisher@testdlab.com>
2023-05-14 22:00:02 +0700

    Add command microservice

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,16 @@
+namespace CommandsService.Controllers;
+
+[Route("api/c/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    public PlatformsController()
+    {
+
+    }
+
+    public ActionResult TestInboundConnection()
+    {
+        
+    }
+}
